a2b62f5e26242387b4a1fdd4dec3952214eeabb8
MarcosLealDev <marcosleal@gmail.com>
2023-10-15 18:33:43 -0400

    more changes

--- src/main/java/com/bellacollezione/todolist/filter/FilterTaskAuth.java
+++ src/main/java/com/bellacollezione/todolist/filter/FilterTaskAuth.java
@@ -26,7 +26,7 @@ public class FilterTaskAuth extends OncePerRequestFilter {
       throws ServletException, IOException {
 
         var servletPath = request.getServletPath();
-        if (servletPath.equals("/tasks/")) {
+        if (servletPath.startsWith("/tasks/")) {
 
           // Get the authentication (username and password)
           var authorization = request.getHeader("Authorization");
